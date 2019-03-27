a8b929021801a5feaae14625f2209c197aefe6bf
Wikonator <topes.jebal@gmail.com>
2019-03-27 23:05:54 +0100

    getting reactive

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Ahoj Natty - chceš si so mnou zatancovať? :)</p>
+          <p>Time to get Reactive</p>
           <a
             className="App-link"
             href="https://reactjs.org"
